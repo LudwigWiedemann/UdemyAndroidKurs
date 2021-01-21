0cd332f54c92bcb7b96e7fa635d9fde4073ee182
Ludwig Wiedemann <ludwigwwiedemann@gmail.com>
2021-01-21 16:33:25 +0100

    QuizFragenActivity

--- QuizApp/app/src/main/java/com/ludwigbmw/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/ludwigbmw/quizapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.ludwigbmw.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hideStatusBar()
+
+        btnStart.setOnClickListener {
+            if ()
+        }
+    }
+
+    fun hideStatusBar() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
